fae9fa73e4a644747186e1c3246c4429c50a27b8
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-05-25 13:25:33 +0200

    Update shop.blade.php

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HOLAZOMI</title> 
-        <link id="color-link" rel="stylesheet" type="text/css" href="{{ asset('assets/css/demo2.css') }}">
+        <link id="color-link" rel="stylesheet" type="text/css" href="{{ asset('assets/css/demo2. css') }}">
  
 </head>
 <style>
