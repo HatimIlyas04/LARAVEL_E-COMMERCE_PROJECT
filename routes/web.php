689e5a5f09f8ab7402f0ff4d274e36ce0bd0573a
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-05-31 12:56:19 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\appController;
+use App\Http\Controllers\appController; 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController; 
