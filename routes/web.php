e5497119c84763f7ce72cbd150f55fb1f151f246
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-05-31 12:55:26 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\appController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController; 
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;  
 use Illuminate\Support\Facades\Route; 
 
 Route::get('/', [appController::class , 'index'])->name('app.index');
