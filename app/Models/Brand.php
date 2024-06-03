2043243299fb0c1eec69d5e2d7a001521944d06a
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-06-03 15:47:15 +0200

    Update Brand.php

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;   
 use Illuminate\Database\Eloquent\Model; 
 
-class Brand extends Model
+class Brand extends Model 
 {
     use HasFactory; 
     function product (){ 
